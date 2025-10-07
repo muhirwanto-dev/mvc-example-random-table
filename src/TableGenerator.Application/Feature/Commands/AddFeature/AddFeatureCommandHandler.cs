6d02d52888424f80b1de2948240dd688fe7187a9
muhirwanto-dev <muhammadirwanto.dev@gmail.com>
2025-10-07 22:34:09 +0700

    feat: show table at the page

--- src/TableGenerator.Application/Feature/Commands/AddFeature/AddFeatureCommandHandler.cs
+++ src/TableGenerator.Application/Feature/Commands/AddFeature/AddFeatureCommandHandler.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using TableGenerator.Application.Interfaces;
 using TableGenerator.Contracts.Dtos.Feature;
+using TableGenerator.Domain.Core.Entities;
 
 namespace TableGenerator.Application.Feature.Commands.AddFeature
 {
@@ -11,7 +12,7 @@ namespace TableGenerator.Application.Feature.Commands.AddFeature
     {
         public Task<ErrorOr<FeatureResponseDto>> Handle(AddFeatureCommand request, CancellationToken cancellationToken)
         {
-            return Task.FromResult(ErrorOrFactory.From(_mapper.Map<Gender, FeatureResponseDto>(new Gender { Description = "Template added" })));
+            return Task.FromResult(ErrorOrFactory.From(_mapper.Map<Gender, FeatureResponseDto>(new Gender { Name = "Template added" })));
         }
     }
 }
