b55a24035fe6f737eabaf36a95c3fcfcc7b0b725
muhirwanto-dev <muhammadirwanto.dev@gmail.com>
2025-10-07 15:07:19 +0700

    root: initial commit the project

---
+++ src/TableGenerator.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TableGenerator.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
